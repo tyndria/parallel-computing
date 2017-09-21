d727419228ef5d4fcac2ab59cc3d745c0b5c0259
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2017-09-21 22:38:00 +0300

    Reduce time of block multiplication; refactor code

--- Matrix Multipication/matrixmultiplication.cpp
+++ Matrix Multipication/matrixmultiplication.cpp
@@ -11,111 +11,90 @@
 
 using namespace std;
 
-long long RunSequencialMultiplication(int n1, int n2, int n3);
-long long RunBlockMultiplication(int n1, int n2, int n3, int block_side);
-vector<int> MultipleMatrixParallel(vector<int> m1, vector<int> m2, int n1, int n2, int n3, int chunk);
-long long RunParallelSequentialMultiplication(int n1, int n2, int n3, int chunk);
-long long RunBlockMultiplicationParallel(int n1, int n2, int n3, int block_side, int chunk);
-vector<vector<int>> MultipleMatrixBlocksParallel(vector<vector<int>> m1_blocks, vector<vector<int>> m2_blocks, int n1, int n2, int n3, int block_size, int chunk);
+long long RunSequencialMultiplication(int n1, int n2, int n3, bool is_parallel_mode, int chunk = 0);
+long long RunBlockMultiplication(int n1, int n2, int n3, int block_side, bool is_parallel_mode, int chunk = 0);
+vector<long> MultipleMatrix(vector<long> m1, vector<long> m2, int n1, int n2, int n3, bool is_parallel_mode, int chunk = 0);
+vector<vector<long>> MultipleMatrixBlocks(vector<vector<long>> &result, vector<vector<long>> m1_blocks, vector<vector<long>> m2_blocks, int n1, int n2, int n3, int block_size, bool is_parallel_mode, int chunk = 0);
 int GenerateRandomValue();
-void GenerateMatrix(int row_number, int column_number, vector<int> &m);
-void PrintMatrix(int row_number, int column_number, vector<int> m);
-vector<int> MultipleMatrix(vector<int> m1, vector<int> m2, int n1, int n2, int n3);
-vector<vector<int>> MultipleMatrixBlocks(vector<vector<int>> m1_blocks, vector<vector<int>> m2_blocks, int n1, int n2, int n3, int block_size);
-vector<int> AddMatrixes(vector<int> m1, vector<int> m2, int row_number, int column_number);
-void GenerateMatrixBlocks(vector<vector<int>> &m_blocks, int row_number, int column_number, int block_side);
+void GenerateMatrix(int row_number, int column_number, vector<long> &m);
+void PrintMatrix(int row_number, int column_number, vector<long> m);
+void AddMatrixes(vector<long> &m1, vector<long> m2, int row_number, int column_number);
+void GenerateMatrixBlocks(vector<vector<long>> &m_blocks, int row_number, int column_number, int block_side);
 
 int main() {
-	
 	const int n = 100, m = 100;
-	const int block_size = 10;
+	const int block_size = 20;
 
-	long long sequentialTime = RunSequencialMultiplication(n, n, n);
-	long long blockTime = RunBlockMultiplication(n, n, n, block_size);
-	long long sequentialParallelTime = RunParallelSequentialMultiplication(n, n, n, 10);
-	long long blockParallelTime = RunBlockMultiplicationParallel(n, n, n, block_size, 10);
-
+	long long sequentialTime = RunSequencialMultiplication(n, n, n, false);
 	cout << "sequential time " << sequentialTime << endl;
+
+	long long blockTime = RunBlockMultiplication(n, n, n, block_size, false);
 	cout << "block time " << blockTime << endl;
+	
+	long long sequentialParallelTime = RunSequencialMultiplication(n, n, n, true, 1);
 	cout << "sequential parallel time " << sequentialParallelTime << endl;
-	cout << "block parallel time " << blockParallelTime << endl;
+	
+	long long blockParallelTime = RunBlockMultiplication(n, n, n, block_size, true, 1);
+	cout << "block parallel time " << blockParallelTime << endl; 
 	
 	system("pause");
     return 0;
 }
 
-long long RunSequencialMultiplication(int n1, int n2, int n3) {
-	vector<int> m1 = {}, m2 = {}, m3 = {};
+long long RunSequencialMultiplication(int n1, int n2, int n3, bool is_parallel_mode, int chunk) {
+	vector<long> m1 = {}, m2 = {}, m3 = {};
 	m3.resize(n1, n3);
 
 	GenerateMatrix(n1, n2, m1);
 	GenerateMatrix(n2, n3, m2);
 
 	auto start = chrono::steady_clock::now();
-	m3 = MultipleMatrix(m1, m2, n1, n2, n3);
+	m3 = MultipleMatrix(m1, m2, n1, n2, n3, is_parallel_mode, chunk);
 	auto duration = chrono::duration_cast<chrono::milliseconds>
                             (std::chrono::steady_clock::now() - start);
 	return duration.count();
 }
 
-long long RunBlockMultiplication(int n1, int n2, int n3, int block_side) {
-	vector<vector<int>> m1_blocks = {};
-	vector<vector<int>> m2_blocks = {};
-	vector<vector<int>> m3_blocks = {};
+long long RunBlockMultiplication(int n1, int n2, int n3, int block_side, bool is_parallel_mode, int chunk) {
+	vector<vector<long>> m1_blocks = {};
+	vector<vector<long>> m2_blocks = {};
+	vector<vector<long>> m3_blocks = {};
+	m3_blocks.resize(n1 * n3);
 
-	GenerateMatrixBlocks(m1_blocks, n1, n2, block_side);
-	GenerateMatrixBlocks(m2_blocks, n2, n3, block_side);
+	for (int i = 0; i < n1; i++) {
+		for (int j = 0; j < n3; j++) {
+			m3_blocks[i * n1 + j].resize(block_side * block_side);
+		}
+	}
+
+	GenerateMatrixBlocks(m1_blocks, n1 / block_side, n2 / block_side, block_side);
+	GenerateMatrixBlocks(m2_blocks, n2 / block_side, n3 / block_side, block_side);
 
 	auto start = chrono::steady_clock::now();
-	m3_blocks = MultipleMatrixBlocks(m1_blocks, m2_blocks, n1/block_side, n2/block_side, n3/block_side, block_side);
+
+	MultipleMatrixBlocks(m3_blocks, m1_blocks, m2_blocks, n1 / block_side, n2 / block_side, n3 / block_side, block_side, is_parallel_mode, chunk);
+	
+	/*for (int i = 0; i < n1 * n3; i++) {
+		PrintMatrix(block_side, block_side, m3_blocks[i]);
+	}*/
+
 	auto duration = chrono::duration_cast<chrono::milliseconds>
 		(std::chrono::steady_clock::now() - start);
 	
 	return duration.count();
 }
 
-long long RunBlockMultiplicationParallel(int n1, int n2, int n3, int block_side, int chunk) {
-	vector<vector<int>> m1_blocks = {};
-	vector<vector<int>> m2_blocks = {};
-	vector<vector<int>> m3_blocks = {};
-
-	GenerateMatrixBlocks(m1_blocks, n1, n2, block_side);
-	GenerateMatrixBlocks(m2_blocks, n2, n3, block_side);
-
-	auto start = chrono::steady_clock::now();
-	m3_blocks = MultipleMatrixBlocksParallel(m1_blocks, m2_blocks, n1 / block_side, n2 / block_side, n3 / block_side, block_side, chunk);
-	auto duration = chrono::duration_cast<chrono::milliseconds>
-		(std::chrono::steady_clock::now() - start);
-
-	return duration.count();
-}
-
-void GenerateMatrixBlocks(vector<vector<int>> &m_blocks, int row_number, int column_number, int block_side) {
+void GenerateMatrixBlocks(vector<vector<long>> &m_blocks, int row_number, int column_number, int block_side) {
 	m_blocks.resize(row_number * column_number * block_side);
 
-	int m_blocks_number = row_number * column_number / (block_side * block_side);
+	long m_blocks_number = row_number * column_number;
 
 	for (int i = 0; i < m_blocks_number; i++) {
-		vector<int> block = {};
+		vector<long> block = {};
 		GenerateMatrix(block_side, block_side, block);
 		m_blocks[i] = block;
+		//PrintMatrix(block_side, block_side, block);
 	}
-}
-
-long long RunParallelSequentialMultiplication(int n1, int n2, int n3, int chunk) {
-	vector<int> m1 = {}, m2 = {}, m3 = {};
-	m3.resize(n1, n3);
-
-	GenerateMatrix(n1, n2, m1);
-	GenerateMatrix(n2, n3, m2);
-
-	auto start = chrono::steady_clock::now();
-
-	m3 = MultipleMatrixParallel(m1, m2, n1, n2, n3, chunk);
-
-	auto duration = chrono::duration_cast<chrono::milliseconds>
-		(std::chrono::steady_clock::now() - start);
-	return duration.count();
 }
 
 int GenerateRandomValue() {
@@ -123,7 +102,7 @@ int GenerateRandomValue() {
 	return (rand() % 100) * sign;
 }
 
-void GenerateMatrix(int row_number, int column_number, vector<int> &m) {
+void GenerateMatrix(int row_number, int column_number, vector<long> &m) {
 	m.resize(row_number * column_number);
 	for (int i = 0; i < row_number; i++) {
 		for (int j = 0; j < column_number; j++) {
@@ -132,7 +111,7 @@ void GenerateMatrix(int row_number, int column_number, vector<int> &m) {
 	}
 }
 
-void PrintMatrix(int row_number, int column_number, vector<int> m) {
+void PrintMatrix(int row_number, int column_number, vector<long> m) {
 	for (int i = 0; i < row_number; i++) {
 		for (int j = 0; j < column_number; j++) {
 			std::cout << m[i * row_number + j] << " ";
@@ -142,93 +121,70 @@ void PrintMatrix(int row_number, int column_number, vector<int> m) {
 	std::cout << "\n";
 }
 
-vector<int> MultipleMatrix(vector<int> m1, vector<int> m2, int n1, int n2, int n3) {
-	vector<int> result_matrix = {};
-	result_matrix.resize(n1 * n3);
-	for (int i = 0; i < n1; i++) {
-		for (int j = 0; j < n3; j++) {
-			int row_sum = 0;
-			for (int k = 0; k < n2; k++) {
-				row_sum += m1[i * n1 + k] * m2[k * n2 + j];
-			}
-			result_matrix[i * n1 + j] = row_sum;
-		}
-	}
-	return result_matrix;
-}
-
-vector<int> MultipleMatrixParallel(vector<int> m1, vector<int> m2, int n1, int n2, int n3, int chunk) {
-	int i, j, k, row_sum;
-	vector<int> result_matrix = {};
+vector<long> MultipleMatrix(vector<long> m1, vector<long> m2, int n1, int n2, int n3, bool is_parallel_mode, int chunk) {
+	int i, j, k;
+	vector<long> result_matrix = {};
 	result_matrix.resize(n1 * n3);
 
-	#pragma omp parallel shared(result_matrix, m1, m2, n1, n2, n3, chunk) private(i, j, k, row_sum) 
-	#pragma omp for schedule (static, chunk)
+	if (!is_parallel_mode) {
 		for (i = 0; i < n1; i++) {
 			for (j = 0; j < n3; j++) {
-				row_sum = 0;
 				for (k = 0; k < n2; k++) {
-					row_sum += m1[i * n1 + k] * m2[k * n2 + j];
+					result_matrix[i * n1 + j] += m1[i * n1 + k] * m2[k * n2 + j];
 				}
-				result_matrix[i * n1 + j] = row_sum;
 			}
-		}
-	return result_matrix;
-}
-
-vector<vector<int>> MultipleMatrixBlocks(vector<vector<int>> m1_blocks, vector<vector<int>> m2_blocks, int n1, int n2, int n3, int block_size) {
-	vector<vector<int>> result_matrix = {};
-	result_matrix.resize(n1 * n3 * block_size);
-	for (int i = 0; i < n1; i++) {
-		for (int j = 0; j < n3; j++) {
-			vector<int> result_matrix_block = {};
-			result_matrix_block.resize(block_size * block_size);
-			for (int k = 0; k < n2; k++) {
-				vector<int> matrix_production = {};
-				matrix_production.resize(block_size * block_size);
-				matrix_production = MultipleMatrix(m1_blocks[i * n1 + k], m2_blocks[k * n2 + j], block_size, block_size, block_size);
-				result_matrix_block = AddMatrixes(result_matrix_block, matrix_production, 2, 2);
-			}
-			result_matrix[i * n1 + j] = result_matrix_block;
 		}
 	}
+	else {
+		#pragma omp parallel shared(result_matrix, m1, m2, n1, n2, n3, chunk) private(i, j, k) 
+		#pragma omp for schedule (static, chunk)
+		for (i = 0; i < n1; i++) {
+			for (j = 0; j < n3; j++) {
+				for (k = 0; k < n2; k++) {
+					result_matrix[i * n1 + j] += m1[i * n1 + k] * m2[k * n2 + j];
+				}
+			}
+		}
+	}
+			
 	return result_matrix;
 }
 
-vector<vector<int>> MultipleMatrixBlocksParallel(vector<vector<int>> m1_blocks, vector<vector<int>> m2_blocks, int n1, int n2, int n3, int block_size, int chunk) {
+vector<vector<long>> MultipleMatrixBlocks(vector<vector<long>> &result_matrix, vector<vector<long>> m1_blocks, vector<vector<long>> m2_blocks, int n1, int n2, int n3, int block_size, bool is_parallel_mode, int chunk) {
 	int i, j, k;
-	vector<int> result_matrix_block, matrix_production;
-	vector<vector<int>> result_matrix = {};
-	result_matrix.resize(n1 * n3 * block_size);
 
-	#pragma omp parallel shared(result_matrix, m1_blocks, m2_blocks, block_size, n1, n2, n3, chunk) private(i, j, k, result_matrix_block, matrix_production) 
-	#pragma omp for schedule (static, chunk)
-	for (i = 0; i < n1; i++) {
-		for (j = 0; j < n3; j++) {
-			result_matrix_block = {};
-			result_matrix_block.resize(block_size * block_size);
-			for (k = 0; k < n2; k++) {
-				matrix_production = {};
-				matrix_production.resize(block_size * block_size);
-				matrix_production = MultipleMatrix(m1_blocks[i * n1 + k], m2_blocks[k * n2 + j], block_size, block_size, block_size);
-				result_matrix_block = AddMatrixes(result_matrix_block, matrix_production, 2, 2);
+	if (!is_parallel_mode) {
+		for (i = 0; i < n1; i++) {
+			for (j = 0; j < n3; j++) {
+				for (k = 0; k < n2; k++) {
+					AddMatrixes(result_matrix[i * n1 + j],
+						MultipleMatrix(m1_blocks[i * n1 + k], m2_blocks[k * n2 + j], block_size, block_size, block_size, false), block_size, block_size);
+				}
 			}
-			result_matrix[i * n1 + j] = result_matrix_block;
+		}
+	}
+	else {
+		#pragma omp parallel shared(result_matrix, m1_blocks, m2_blocks, block_size, n1, n2, n3, chunk) private(i, j, k) 
+		#pragma omp for schedule (static, chunk)
+		for (i = 0; i < n1; i++) {
+			for (j = 0; j < n3; j++) {
+				for (k = 0; k < n2; k++) {
+					AddMatrixes(result_matrix[i * n1 + j],
+						MultipleMatrix(m1_blocks[i * n1 + k], m2_blocks[k * n2 + j], block_size, block_size, block_size, true, chunk), block_size, block_size);
+				}
+			}
 		}
 	}
 	return result_matrix;
 }
 
 
-vector<int> AddMatrixes(vector<int> m1, vector<int> m2, int row_number, int column_number) {
-	vector<int> result_matrix = {};
-	result_matrix.resize(row_number * column_number);
+void AddMatrixes(vector<long> &m1, vector<long> m2, int row_number, int column_number) {
 	for (int i = 0; i < row_number; i++) {
 		for (int j = 0; j < column_number; j++) {
-			result_matrix[i * row_number + j] = m1[i * row_number + j] + m2[i * row_number + j];
+			m1[i * row_number + j] = m1[i * row_number + j] + m2[i * row_number + j];
 		}
 	}
-	return result_matrix;
 }
 
 
